d3434c8ec2562c281717986ff02243d9a8bc5927
tommy martinez <thomasjohnmartinez@gmail.com>
2025-09-18 00:15:26 -0400

    fixed youtube embed

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     <>
      <div >
       <p>Distant Streams</p>
-      <iframe width="560" height="315" src="https://youtube.com/live/sXD6_vs-7FI?feature=share" frameborder="0" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/0kebaNrgCA4?si=IPw_dESG9cYfMGg9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
 
       
